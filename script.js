3c6619ed4790b985d2fdab48fa3702f676c79556
dhiemily <167090493+dhiemily@users.noreply.github.com>
2026-08-24 12:34:05 -0300

    s

--- script.js
+++ script.js
@@ -6,51 +6,34 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
   {
-    enunciado: "Pergunta 1",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "Você tem mais amigos da sua idade ou com uma faixa etária muito diferente da sua?",
+    alternativas: ["a)sim", "b)não"],
   },
   {
-    enunciado: "Pergunta 2",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "O etarismo (ou preconceito de idade) costuma criar barreiras sociais que dificultam a aproximação e a amizade entre pessoas de gerações diferentes?",
+    alternativas: ["a)verdadeiro", "b)falso"],
   },
 
 
  {
-    enunciado: "Pergunta 1",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "Amizades que cruzam barreiras de idade (intergeracionais) trazem benefícios mútuos, permitindo a troca de experiências e a quebra de preconceitos etários?",
+    alternativas: ["a)verdadeiro", "b)falso"],
   },
   {
-    enunciado: "Pergunta 2",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "Afirmar rigidamente que 'jovens só devem andar com jovens e velhos só com velhos' é uma ideia que reforça a segregação imposta pelo etarismo na sociedade?",
+    alternativas: ["a)verdadeira", "b)falso"],
   },
 
 
    {
-    enunciado: "Pergunta 1",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "O lema 'para as amizades, as idades não importam' desafia a lógica do etarismo ao valorizar a conexão humana e a afinidade pessoal acima de rótulos geracionais?",
+    alternativas: ["a)verdadeiro", "b)falso"],
   },
   {
-    enunciado: "Pergunta 2",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "O incentivo a atividades comunitárias que integram diferentes faixas etárias ajuda a reduzir a solidão e a construir uma sociedade menos egoísta?
+,
+    alternativas: ["a)verdadeiro", "b)falso"],
   },
 
-
-   {
-    enunciado: "Pergunta 1",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
-  },
-  {
-    enunciado: "Pergunta 2",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
-  },
-
-  
-   {
-    enunciado: "Pergunta 1",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
-  },
-  {
-    enunciado: "Pergunta 2",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
-  },
 ];
+
